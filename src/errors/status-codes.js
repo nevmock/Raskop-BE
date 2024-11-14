82bb7dc0804dd3dcf5e3f25a1b5dd065abe8d665
mhndkptr <muhamadhendika77@gmail.com>
2024-11-15 00:27:39 +0700

    upd: errors

---
+++ src/errors/status-codes.js
@@ -0,0 +1,30 @@
+export default {
+  BAD_REQUEST: {
+    code: 400,
+    message: "Bad Request",
+  },
+  NOT_FOUND: {
+    code: 404,
+    message: "Not Found",
+  },
+  INTERNAL_SERVER: {
+    code: 500,
+    message: "Internal Server Error",
+  },
+  UNAUTHORIZE: {
+    code: 401,
+    message: "User Unauthorize",
+  },
+  FORBIDDEN: {
+    code: 403,
+    message: "No Access",
+  },
+  DUPLICATE: {
+    code: 450,
+    message: "Duplicate Found",
+  },
+  INVALID_PARAMS: {
+    code: 477,
+    message: "Params Format Not Registered In Application",
+  },
+};
