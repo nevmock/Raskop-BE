9dc9c3cccf0fb002094986d963e928caf7fb2314
kevinirawand <kevinid07@gmail.com>
2024-12-20 02:18:06 +0700

    RSKP-20 | Deploy BE Server Dev | General

--- src/app.js
+++ src/app.js
@@ -95,4 +95,5 @@ class ExpressApplication {
   }
 }
 
+
 export default ExpressApplication;
--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ const PORT = process.env.PORT || 3000;
 const app = new ExpressApplication(PORT);
 const server = app.start();
 
+
 process.on("SIGTERM", () => {
   logger.warn('SIGTERM RECIEVED!');
   server.close(() => {
