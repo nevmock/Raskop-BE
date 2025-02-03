3c41328086bf5c47b74cbf7366e6c047327c643a
mhndkptr <muhamadhendika77@gmail.com>
2025-02-04 04:58:58 +0700

    RSKP-26 | Add logic for field merged_available & finish | Meja

--- src/domains/table/table-controller.js
+++ src/domains/table/table-controller.js
@@ -1,63 +1,59 @@
-import { createdResponse, successResponse } from '../../utils/response.js';
-import TableServices from './table-services.js';
+import { createdResponse, successResponse } from "../../utils/response.js";
+import TableServices from "./table-services.js";
 
 class TableController {
+  async index(req, res) {
+    let params = req.query;
 
-    async index(req, res) {
-        let params = req.query;
-    
-        const { data, total } = await TableServices.getAll(params);
-    
-        return successResponse(res, data, total);
+    const { data, total } = await TableServices.getAll(params);
+
+    return successResponse(res, data, total);
+  }
+
+  async show(req, res) {
+    const { id } = req.params;
+
+    if (!id) {
+      throw BaseError.badRequest("ID is required");
     }
 
-    async show(req, res){
-        const { id } = req.params;
+    const table = await TableServices.getById(id);
 
-        if (!id) {
-            throw BaseError.badRequest("ID is required");
-        }
-    
-        const table = await TableServices.getById(id);
-    
-        return successResponse(res, table);
-    }
-  
-  
-    async createOrUpdate(req, res) {
-      let data = req.body;
+    return successResponse(res, table);
+  }
 
-      if (req.file){
-        const imageUri = `/images/table/${req.file.filename}`;
-        data.imageUri = imageUri;
-      }
-      
-      if (data.id) {
-        const table = await TableServices.update(data.id, data, req.file);
-  
-        return successResponse(res, table);
-      }
-      
-      const table = await TableServices.create(data);
-  
-      return createdResponse(res, table);
+  async createOrUpdate(req, res) {
+    let data = req.body;
+
+    if (req.file) {
+      const imageUri = `/images/table/${req.file.filename}`;
+      data.imageUri = imageUri;
     }
-  
-    async delete(req, res) {
-      const { id, permanent } = req.query;
-  
-      if (permanent === true || permanent === "true") {
-  
-        await TableServices.deletePermanent(id);
-  
-        return successResponse(res, "Table deleted permanently");
-      }
-  
-      await TableServices.delete(id);
-  
-      return successResponse(res, "Table deleted successfully");
+
+    if (data.id) {
+      const table = await TableServices.update(data.id, data, req.file);
+
+      return successResponse(res, table);
     }
-  
+
+    const table = await TableServices.create(data);
+
+    return createdResponse(res, table);
+  }
+
+  async delete(req, res) {
+    const { id, permanent } = req.query;
+
+    if (permanent === true || permanent === "true") {
+      await TableServices.deletePermanent(id);
+
+      return successResponse(res, "Table deleted permanently");
+    }
+
+    await TableServices.delete(id);
+
+    return successResponse(res, "Table deleted successfully");
+  }
 }
 
 export default new TableController();
--- src/domains/table/table-services.js
+++ src/domains/table/table-services.js
@@ -6,171 +6,200 @@ import { snakeCase } from "change-case";
 import { deleteFileIfExists } from "../../utils/delete-file.js";
 
 class TableServices {
-    constructor() {
-        this.TableRepository = TableRepository;
-    }
-
-    getAll = async (params = {}) => {
-        let {
-            start = 1,
-            length = 10,
-            search = '',
-            advSearch,
-            order,
-        } = params;
-
-        start = JSON.parse(start);
-        length = JSON.parse(length);
-
-        advSearch = (advSearch) ? JSON.parse(advSearch) : null;
-        order = (order) ? JSON.parse(order) : null;
-
-        const where = {
-            ...(search && {
-                OR: [
-                    { barcode: { contains: search } },
-                    { no_table: { contains: search } },
-                    { description: { contains: search } },
-                ],
-            }),
-            ...(advSearch && {
-                ...(advSearch.id && { id: { contains: advSearch.id }}),
-                ...(advSearch.minCapacity && { min_capacity: advSearch.minCapacity }),
-                ...(advSearch.maxCapacity && { max_capacity: advSearch.maxCapacity }),
-                ...(advSearch.description && { description: { contains: advSearch.description } }),
-                ...(advSearch.noTable && { no_table: { contains: advSearch.noTable } }),
-                ...(advSearch.isOutdoor !== undefined && { is_outdoor: advSearch.isOutdoor }),
-                ...(advSearch.isActive !== undefined && { is_active: advSearch.isActive }),
-                ...((advSearch.withDeleted === "false" || advSearch.withDeleted === false) && { deleted_at: null }),
-                ...((advSearch.startDate || advSearch.endDate) && {
-                    created_at: {
-                        ...(advSearch.startDate && { gte: new Date(advSearch.startDate) }),
-                        ...(advSearch.endDate && { lte: new Date(advSearch.endDate) }),
-                    },
-                }),
-            }),
-        };
-
-        const orderBy = Array.isArray(order) ? order.map(o => ({
-            [snakeCase(o.column)]: o.direction.toLowerCase() === 'asc' ? 'asc' : 'desc',
-        })) : [];
-
-        const filters = {
-            where,
-            orderBy,
-            skip: start - 1,
-            take: length,
-        };
-
-        let tables = await this.TableRepository.get(filters);
-
-        tables = camelize(tables);
-
-        return tables;
-    };
-
-    getById = async (id, params = {}) => {
-        let table = await this.TableRepository.getById(id, {
-            ...params,
-        });
-
-        if (!table) {
-            throw BaseError.notFound("Table does not exist");
-        }
-    
-        table = camelize(table);
-    
-        return table;
-    };
-
-    getByNoTable = async (noTable) => {
-        let table = await this.TableRepository.getByNoTable(noTable);
-
-        table = camelize(table);
-
-        return table;
-    }
-
-    create = async (data) => {
-        data = convertKeysToSnakeCase(data);
-
-        const isNoTableExist = await this.TableRepository.getByNoTable(data.no_table);
-
-        if (isNoTableExist) {
-            deleteFileIfExists(data.image_uri);
-            throw BaseError.badRequest("No Table already exist");
-        }
-
-        let table = await this.TableRepository.create(data);
-
-        table = camelize(table);
-
-        return table;
-    }
-
-    update = async (id, data, file) => {
-        const isExist = await this.TableRepository.getById(id);
-
-        if (!isExist) {
-            throw BaseError.notFound("Table does not exist");
-        }
-        data = convertKeysToSnakeCase(data)
-
-        const isNoTableExist = await this.TableRepository.getByNoTable(data.no_table);
-
-        if (isNoTableExist && isNoTableExist.id !== id) {
-            deleteFileIfExists(data.image_uri);
-            throw BaseError.badRequest("No Table already exist");
-        }
-
-        if (file && isExist.image_uri) {
-            deleteFileIfExists(isExist.image_uri);
-        }
-
-        let table = await this.TableRepository.update(id, data);
-
-        table = camelize(table);
-
-        return table;
-    }
-
-    delete = async (id) => {
-        const params = {
-            where: {
-                deleted_at: null,
-            },
-        }
-
-        const isExist = await this.TableRepository.getById(id, params);
-
-        if (!isExist) {
-            throw BaseError.notFound("Table does not exist");
-        }
-
-        await this.TableRepository.delete(id);
-    }
-
-    deletePermanent = async (id) => {
-        const isExist = await this.TableRepository.getById(id, { include : { detail_reservasis: true } });
-
-        if (!isExist) {
-            throw BaseError.notFound("Table does not exist");
-        }
-
-        if (!isExist.deleted_at) {
-            throw BaseError.badRequest("Table is not deleted yet");
-        }
-
-        if (isExist.detail_reservasis.length > 0) {
-            throw BaseError.badRequest("Table cannot be deleted permanently because there are reservasions that use this Table");
-        }
-
-        if (isExist.image_uri) {
-            deleteFileIfExists(isExist.image_uri);
-        }
-
-        await this.TableRepository.deletePermanent(id);
-    }
+  constructor() {
+    this.TableRepository = TableRepository;
   }
-  
-  export default new TableServices();
+
+  getAll = async (params = {}) => {
+    let { start = 1, length = 10, search = "", advSearch, order } = params;
+
+    start = JSON.parse(start);
+    length = JSON.parse(length);
+
+    advSearch = advSearch ? JSON.parse(advSearch) : null;
+    order = order ? JSON.parse(order) : null;
+
+    const where = {
+      ...(search && {
+        OR: [
+          { barcode: { contains: search } },
+          { no_table: { contains: search } },
+          { description: { contains: search } },
+        ],
+      }),
+      ...(advSearch && {
+        ...(advSearch.id && { id: { contains: advSearch.id } }),
+        ...(advSearch.minCapacity && { min_capacity: advSearch.minCapacity }),
+        ...(advSearch.maxCapacity && { max_capacity: advSearch.maxCapacity }),
+        ...(advSearch.description && { description: { contains: advSearch.description } }),
+        ...(advSearch.noTable && { no_table: { contains: advSearch.noTable } }),
+        ...(advSearch.isOutdoor !== undefined && { is_outdoor: advSearch.isOutdoor }),
+        ...(advSearch.isActive !== undefined && { is_active: advSearch.isActive }),
+        ...((advSearch.withDeleted === "false" || advSearch.withDeleted === false) && { deleted_at: null }),
+        ...((advSearch.startDate || advSearch.endDate) && {
+          created_at: {
+            ...(advSearch.startDate && { gte: new Date(advSearch.startDate) }),
+            ...(advSearch.endDate && { lte: new Date(advSearch.endDate) }),
+          },
+        }),
+      }),
+    };
+
+    const orderBy = Array.isArray(order)
+      ? order.map((o) => ({
+          [snakeCase(o.column)]: o.direction.toLowerCase() === "asc" ? "asc" : "desc",
+        }))
+      : [];
+
+    const filters = {
+      where,
+      orderBy,
+      skip: start - 1,
+      take: length,
+    };
+
+    let tables = await this.TableRepository.get(filters);
+
+    tables = camelize(tables);
+
+    return tables;
+  };
+
+  getById = async (id, params = {}) => {
+    let table = await this.TableRepository.getById(id, {
+      ...params,
+    });
+
+    if (!table) {
+      throw BaseError.notFound("Table does not exist");
+    }
+
+    table = camelize(table);
+
+    return table;
+  };
+
+  getByNoTable = async (noTable) => {
+    let table = await this.TableRepository.getByNoTable(noTable);
+
+    table = camelize(table);
+
+    return table;
+  };
+
+  create = async (data) => {
+    data = convertKeysToSnakeCase(data);
+
+    const isNoTableExist = await this.TableRepository.getByNoTable(data.no_table);
+
+    if (isNoTableExist) {
+      deleteFileIfExists(data.image_uri);
+      throw BaseError.badRequest("No Table already exist");
+    }
+
+    if (data.merged_available && data.merged_available.length > 0) {
+      await Promise.all(
+        data.merged_available.map(async (tableId) => {
+          const isIdTableExist = await TableRepository.getById(tableId);
+          if (!isIdTableExist) {
+            deleteFileIfExists(data.image_uri);
+            throw BaseError.badRequest(`Id Table ${tableId} does not exist`);
+          }
+        })
+      );
+    }
+
+    let table = await this.TableRepository.create(data);
+
+    table = camelize(table);
+
+    return table;
+  };
+
+  update = async (id, data, file) => {
+    const isExist = await this.TableRepository.getById(id);
+
+    if (!isExist) {
+      throw BaseError.notFound("Table does not exist");
+    }
+    data = convertKeysToSnakeCase(data);
+
+    const isNoTableExist = await this.TableRepository.getByNoTable(data.no_table);
+
+    if (isNoTableExist && isNoTableExist.id !== id) {
+      deleteFileIfExists(data.image_uri);
+      throw BaseError.badRequest("No Table already exist");
+    }
+
+    if (data.merged_available && data.merged_available.length > 0) {
+      await Promise.all(
+        data.merged_available.map(async (tableId) => {
+          const isIdTableExist = await TableRepository.getById(tableId);
+          if (!isIdTableExist) {
+            deleteFileIfExists(data.image_uri);
+            throw BaseError.badRequest(`Id Table ${tableId} does not exist`);
+          }
+        })
+      );
+    }
+
+    if (file && isExist.image_uri) {
+      deleteFileIfExists(isExist.image_uri);
+    }
+
+    let table = await this.TableRepository.update(id, data);
+
+    table = camelize(table);
+
+    return table;
+  };
+
+  delete = async (id) => {
+    const params = {
+      where: {
+        deleted_at: null,
+      },
+    };
+
+    const isExist = await this.TableRepository.getById(id, params);
+
+    if (!isExist) {
+      throw BaseError.notFound("Table does not exist");
+    }
+
+    await this.TableRepository.delete(id);
+  };
+
+  deletePermanent = async (id) => {
+    const isExist = await this.TableRepository.getById(id, { include: { detail_reservasis: true } });
+
+    if (!isExist) {
+      throw BaseError.notFound("Table does not exist");
+    }
+
+    if (!isExist.deleted_at) {
+      throw BaseError.badRequest("Table is not deleted yet");
+    }
+
+    if (isExist.detail_reservasis.length > 0) {
+      throw BaseError.badRequest(
+        "Table cannot be deleted permanently because there are reservasions that use this Table"
+      );
+    }
+
+    const tablesUsingThisId = await this.TableRepository.findAllWithMergedAvailable(isExist.id);
+    if (tablesUsingThisId.length > 0) {
+      throw BaseError.badRequest(
+        "Table cannot be deleted permanently because there are merged available that use this Table"
+      );
+    }
+
+    if (isExist.image_uri) {
+      deleteFileIfExists(isExist.image_uri);
+    }
+
+    await this.TableRepository.deletePermanent(id);
+  };
+}
+
+export default new TableServices();
